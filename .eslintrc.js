dca2efd547502eb3d5641238b50f89f49c753d21
Tripouille <jmichel.gambard@gmail.com>
2022-03-22 21:10:25 +0100

    feat(setup): add eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['plugin:react/recommended', 'airbnb-typescript-prettier'],
+  rules: {
+    'react/function-component-definition': 'off',
+    'react/require-default-props': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+  },
+};
