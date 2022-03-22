c05a0c9ea6051f164737ab1689c79b74499963d1
Tripouille <jmichel.gambard@gmail.com>
2022-03-22 21:47:27 +0100

    feat(setup): add mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
-  return <h1>testwe</h1>;
+  return <Button variant="contained">testwe</Button>;
 }
 
 export default App;
