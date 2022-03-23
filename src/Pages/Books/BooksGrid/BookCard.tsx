5cc1864b35b0bca4e3abce6798dc50e8b4f9e48b
Tripouille <jmichel.gambard@gmail.com>
2022-03-23 19:13:58 +0100

    feat(books): create Books page + BooksGrid + BookCard

---
+++ src/Pages/Books/BooksGrid/BookCard.tsx
@@ -0,0 +1,72 @@
+import React, { FC } from 'react';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
+import SearchIcon from '@mui/icons-material/Search';
+import { Box, Button, Card, CardActions, CardContent, Divider, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { Book } from '../../../services/books/types';
+
+const useStyles = makeStyles(() => ({
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '100%',
+    border: '1px solid brown',
+  },
+  cardContent: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+}));
+
+export interface BookCardProps {
+  book: Book;
+}
+
+const BookCard: FC<BookCardProps> = ({ book }) => {
+  const classes = useStyles();
+  const { name, isbn, authors, numberOfPages } = book;
+
+  return (
+    <Card className={classes.card}>
+      <CardContent className={classes.card}>
+        <Typography variant="subtitle1" color="text.primary" textAlign="center" padding={2}>
+          {name}
+        </Typography>
+        <Divider />
+
+        <Box display="flex" alignItems="center" justifyContent="center" flexWrap="wrap" padding={2}>
+          <SearchIcon />
+          <Typography variant="body2" color="text.secondary" textAlign="center" padding={2}>
+            {isbn}
+          </Typography>
+          <AutoStoriesIcon />
+          <Typography variant="body2" color="text.secondary" textAlign="center" padding={2}>
+            {numberOfPages}
+          </Typography>
+        </Box>
+        <Divider />
+
+        <Box display="flex" alignItems="center" justifyContent="center" flexWrap="wrap">
+          <HistoryEduIcon />
+          {authors.map(author => (
+            <Typography key={author} variant="subtitle2" padding={2}>
+              {author}
+            </Typography>
+          ))}
+        </Box>
+        <Divider />
+      </CardContent>
+
+      <CardActions>
+        <Button variant="outlined" color="primary" fullWidth>
+          See related characters
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default BookCard;
---
+++ src/Pages/Books/index.tsx
@@ -0,0 +1,38 @@
+import { Backdrop, CircularProgress, Pagination, Stack, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { useBooksQuery } from '../../services/books';
+import BooksGrid from './BooksGrid';
+
+const Books = () => {
+  const [page, setPage] = useState(1);
+  const { data, isFetching } = useBooksQuery({ page, pageSize: 6 });
+
+  return (
+    <Stack alignItems="center">
+      <Typography variant="h1" color="primary">
+        Books
+      </Typography>
+      {data && (
+        <>
+          <BooksGrid books={data.books} />
+          <Pagination
+            page={page}
+            count={data.totalPages}
+            onChange={(_event, newPage) => setPage(newPage)}
+            variant="outlined"
+            color="primary"
+            shape="rounded"
+            size="large"
+          />
+        </>
+      )}
+      {isFetching && (
+        <Backdrop sx={{ zIndex: theme => theme.zIndex.drawer + 1 }} open>
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      )}
+    </Stack>
+  );
+};
+
+export default Books;
