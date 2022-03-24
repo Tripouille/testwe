a8d3e7d5581f8c33b969e2576111257b00976ee0
Tripouille <jmichel.gambard@gmail.com>
2022-03-24 19:48:18 +0100

    feat(characters): create Characters page CharactersGrid CharacterCard

--- src/Pages/Characters/index.tsx
+++ src/Pages/Characters/index.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import { Backdrop, CircularProgress, Pagination, Stack } from '@mui/material';
+import React, { useState } from 'react';
+import { useCharactersQuery } from '../../services/characters';
+import CharactersGrid from './CharactersGrid';
 
 const Characters = () => {
-  return <h1>Characters</h1>;
+  const [page, setPage] = useState(1);
+  const { data, isFetching } = useCharactersQuery({ page, pageSize: 6 });
+
+  return (
+    <Stack alignItems="center">
+      {data && (
+        <>
+          <CharactersGrid characters={data.characters} />
+          <Pagination
+            page={page}
+            count={data.totalPages}
+            onChange={(_event, newPage) => setPage(newPage)}
+            variant="outlined"
+            color="primary"
+            shape="rounded"
+            size="large"
+          />
+        </>
+      )}
+      {isFetching && (
+        <Backdrop sx={{ zIndex: theme => theme.zIndex.drawer + 1 }} open>
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      )}
+    </Stack>
+  );
 };
 
 export default Characters;
