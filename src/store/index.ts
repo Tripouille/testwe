62037cb042f0778645173cba10088d5cc556beba
Tripouille <jmichel.gambard@gmail.com>
2022-03-24 13:56:53 +0100

    feat(characters): create characters service

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query/react';
 import { booksApi } from '../services/books';
+import { charactersApi } from '../services/characters';
 
 const store = configureStore({
   reducer: {
     // Add the generated reducer as a specific top-level slice
     [booksApi.reducerPath]: booksApi.reducer,
+    [charactersApi.reducerPath]: charactersApi.reducer,
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(booksApi.middleware),
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(booksApi.middleware, charactersApi.middleware),
 });
 
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
