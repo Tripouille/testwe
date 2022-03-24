1bdb67dcb701242471efce037cdd2bcfa87813bf
Tripouille <jmichel.gambard@gmail.com>
2022-03-24 20:39:19 +0100

    refactor(styles): create useCardStyles

---
+++ src/utils/styles.ts
@@ -0,0 +1,11 @@
+import { makeStyles } from '@mui/styles';
+
+// eslint-disable-next-line import/prefer-default-export
+export const useCardStyles = makeStyles(() => ({
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '100%',
+  },
+}));
