6f1fe3ef972e94f543536e28cdf6375d8ad8f968
Tripouille <jmichel.gambard@gmail.com>
2022-03-25 22:04:42 +0100

    refactor(utils): create getLastUrlPart

--- src/components/CharactersGridFromUrls/CharacterCardWrapper.tsx
+++ src/components/CharactersGridFromUrls/CharacterCardWrapper.tsx
@@ -2,13 +2,14 @@ import { Skeleton } from '@mui/material';
 import React, { FC } from 'react';
 import CharacterCard from '../CharacterCard';
 import { useCharacterQuery } from '../../services/characters';
+import { getLastUrlPart } from '../../utils/url';
 
 export interface CharacterCardWrapperProps {
   characterUrl: string;
 }
 
 const CharacterCardWrapper: FC<CharacterCardWrapperProps> = ({ characterUrl }) => {
-  const characterId = Number(characterUrl.split('/').at(-1));
+  const characterId = Number(getLastUrlPart(characterUrl));
   const { data: character, isFetching } = useCharacterQuery({ id: characterId });
 
   if (isFetching) return <Skeleton height={300} width="100%" />;
