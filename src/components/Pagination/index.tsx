db55898fc58c04d4ba159a3a86263aab780e8dd1
Tripouille <jmichel.gambard@gmail.com>
2022-03-27 15:26:23 +0200

    feat(pagination): do not display pagination when no page

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -4,6 +4,7 @@ import React, { FC } from 'react';
 export type PaginationProps = Pick<MuiPaginationProps, 'page' | 'count' | 'onChange'>;
 
 const Pagination: FC<PaginationProps> = ({ page, count, onChange }) => {
+  if (count === 0) return null;
   return (
     <MuiPagination
       page={page}
