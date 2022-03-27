9c20d58276adf79d2c5fe3cf4122242532ec0230
Tripouille <jmichel.gambard@gmail.com>
2022-03-27 13:38:39 +0200

    refactor(cypress): move isAriaCurrent in commands

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,1 +1,5 @@
 Cypress.Commands.add('dataCy', value => cy.get(`[data-cy=${value}]`));
+
+Cypress.Commands.add('isAriaCurrent', { prevSubject: true }, (subject: Cypress.Chainable) => {
+  cy.wrap(subject).should('have.attr', 'aria-current', 'true');
+});
--- cypress/utils/assert.ts
+++ cypress/utils/assert.ts
@@ -1,9 +1,5 @@
 import { FrontOfficeRoutes } from '../../src/utils/constants';
 
-export const isAriaCurrent = (chainable: Cypress.Chainable) => {
-  chainable.should('have.attr', 'aria-current', 'true');
-};
-
 /** Location */
 const currentUrlIs = (pathname: string) => cy.location('pathname').should('eq', pathname);
 export const currentUrlIsHome = () => currentUrlIs('/');
