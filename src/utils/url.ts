2d71548c69c3eecf0d579ca0bc8925adb7c23865
Tripouille <jmichel.gambard@gmail.com>
2022-03-27 15:16:00 +0200

    fix(mobile): fix no results displayed

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/prefer-default-export
 export const getLastUrlPart = (url: string) => {
-  return url.split('/').at(-1) ?? '';
+  return url.split('/').pop() ?? '';
 };
