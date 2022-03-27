29bf04efa423052a6ae09f3702ad1440a69be9be
Tripouille <jmichel.gambard@gmail.com>
2022-03-27 15:56:57 +0200

    feat(favorite-characters): display a message when no favorite

--- src/pages/FavoriteCharacters/index.tsx
+++ src/pages/FavoriteCharacters/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Typography, Box } from '@mui/material';
 import CharactersGridFromUrlsWithPagination from '../../components/CharactersGridFromUrlsWithPagination';
 import useFavoriteCharacterUrls from '../../hooks/useFavoriteCharacterUrls';
 
 const FavoriteCharacters = () => {
   const [favoriteCharacterUrls] = useFavoriteCharacterUrls();
+  if (favoriteCharacterUrls.length === 0)
+    return (
+      <Box display="flex" justifyContent="center">
+        <Typography m={4} variant="body1">
+          You have no favorite yet, you can add one with the star button on top of a character.
+        </Typography>
+      </Box>
+    );
   return <CharactersGridFromUrlsWithPagination charactersUrls={favoriteCharacterUrls} />;
 };
 
